e48a3f14e700b9c8cde898bb79280c2537ae6047
BrandonYEO <yeoph90@gmail.com>
2021-12-20 10:43:39 +0800

    Initial commit

---
+++ Forest Plot.R
@@ -0,0 +1,17 @@
+# Example of FOrest plot in Bioinformatics 
+# https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-019-2744-2/figures/5
+#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6900616/
+
+# Understanding Forest Plot
+#https://s4be.cochrane.org/blog/2016/07/11/tutorial-read-forest-plot/
+
+
+
+
+
+
+
+
+
+
+
